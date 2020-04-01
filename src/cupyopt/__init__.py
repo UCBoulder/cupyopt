06f21584eb2064e32d61da712a036869eb4b72bf
Steve Taylor <stta9820@colorado.edu>
2020-04-01 13:44:40 -0600

    Rename utilities to tasks... too easy to misspell

--- src/cupyopt/__init__.py
+++ src/cupyopt/__init__.py
@@ -1,3 +1,3 @@
-from .sftp_utilities import SFTPGet, SFTPRemove, DFGetOldestFile, SFTPPoll
-from .oradb_utilities import ORADBGetEngine, ORADBSelectToDataFrame
-from .pandera_utilities import PaSchemaFromFile, PaValidate
+from .sftp_tasks import SFTPGet, SFTPRemove, DFGetOldestFile, SFTPPoll
+from .oradb_tasks import ORADBGetEngine, ORADBSelectToDataFrame
+from .pandera_tasks import PaSchemaFromFile, PaValidate
