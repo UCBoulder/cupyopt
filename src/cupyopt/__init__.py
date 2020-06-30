6a078d3c7489f5e2905dc53bf7eaa4524362c066
dabu5788 <dave.bunten@colorado.edu>
2020-06-30 10:26:43 -0600

    Setting up module stuff, removing task classes to reduce confusion. Intent is to use task(function) as possibly needed.

--- src/cupyopt/__init__.py
+++ src/cupyopt/__init__.py
@@ -1,4 +1,4 @@
 from .sftp_tasks import SFTPGet, SFTPPut, SFTPRemove, DFGetOldestFile, SFTPPoll
 from .oradb_tasks import ORADBGetEngine, ORADBSelectToDataFrame
 from .pandas_tasks import PdDataFrameFromCSV, PdDataFrameToCSV, PdDatadictTranslate
-from .pandera_tasks import PaSchemaFromDatadict, PaSchemaFromFile, PaValidate
+from .validation import PaSchema, PaValidate
