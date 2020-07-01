aef47a24073fd3d8c888c314ac1179097fb3b38f
dabu5788 <dave.bunten@colorado.edu>
2020-07-01 12:47:17 -0600

    Casing / naming change

--- src/cupyopt/__init__.py
+++ src/cupyopt/__init__.py
@@ -1,4 +1,4 @@
 from .sftp_tasks import SFTPGet, SFTPPut, SFTPRemove, DFGetOldestFile, SFTPPoll
 from .oradb_tasks import ORADBGetEngine, ORADBSelectToDataFrame
 from .pandas_tasks import PdDataFrameFromCSV, PdDataFrameToCSV, PdDatadictTranslate
-from .validation import PaSchema, PaValidate
+from .validation import pa_schema, pa_validate
--- src/cupyopt/validation.py
+++ src/cupyopt/validation.py
@@ -5,11 +5,11 @@ import pandera as pa
 from box import Box
 
 
-def PaSchema(yaml_schema) -> pa.DataFrameSchema:
+def pa_schema(yaml_schema) -> pa.DataFrameSchema:
     logging.info("Creating Pandera DataFrameSchema from Yaml-based schema.")
     return pa.io.from_yaml(yaml_schema)
 
 
-def PaValidate(df: pd.DataFrame, schema: pa.DataFrameSchema) -> pd.DataFrame:
+def pa_validate(df: pd.DataFrame, schema: pa.DataFrameSchema) -> pd.DataFrame:
     logging.info("Validating Pandas DataFrame using Pandera DataFrameSchema.")
     return schema.validate(df)
