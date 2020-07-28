a09b8c83da195b63bf20547959b126dfb4fdad81
Steve Taylor <staylorx@gmail.com>
2020-07-28 10:07:24 -0600

    Bring up version in setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if os.path.isfile(requirement_path):
 # This call to setup() does all the work
 setuptools.setup(
     name="cupyopt",
-    version="0.0.1",
+    version="0.12-1",
     description="CU Python Opinionated Prefect Tasks",
     long_description=README,
     long_description_content_type="text/x-rst",
