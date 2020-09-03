e1180432d70b94f13cd48a5c05cdc726d00b9e2d
dabu5788 <dave.bunten@colorado.edu>
2020-09-03 16:48:28 -0600

    Initial take on adding avro schema and validation features; addressing cupyopt#9

---
+++ src/cupyopt/schema.py
@@ -0,0 +1,56 @@
+import json
+import pandas as pd
+import fastavro as avro
+import pandavro as pda
+
+
+def avro_schema(avsc: Union[dict, str]) -> dict:
+
+    # if a dictionary type, parse from dict
+    if type(avsc) == dict:
+        return avro.schema.parse_schema(avsc)
+    # if a str type, load from file
+    elif type(avsc) == str:
+        return avro.schema.load_schema(avsc)
+
+
+def infer_df_avro_schema(
+    df: pd.DataFrame,
+    name: str = None,
+    namespace: str = None,
+    times_as_micros: bool = True,
+) -> dict:
+
+    # infer the schema using pandavro
+    schema = pda.__schema_infer(df=df, times_as_micros=times_as_micros)
+
+    # add custom schema name if exists (by default "Root")
+    if name:
+        schema["name"] = name
+    # add custom schema name if exists (by default, non-existent)
+    if namespace:
+        schema["namespace"] = namespace
+
+    return avro_schema(schema)
+
+
+def avro_schema_to_file(avsc: dict, filename: str = None, filedir: str = "./"):
+
+    # infer the filename based on the namespace.name from the avro dict key:values
+    if not filename:
+        filename = avro.schema.fullname(avsc)
+
+    # create internal copy of avro dict so as to not otherwise interfere
+    _avsc = avsc.copy()
+
+    # remove additional keys fastavro places in dict
+    for key in ["__named_schemas", "__fastavro_parsed"]:
+        if key in _avsc.keys():
+            _avsc.pop(key)
+
+    filepath = "{}{}.avsc".format(filedir, filename)
+
+    with open(filepath, "w") as f:
+        f.write(json.dumps(_avsc, indent=4))
+
+    return filepath
--- src/cupyopt/validation.py
+++ src/cupyopt/validation.py
@@ -1,5 +1,5 @@
 import logging
-
+import fastavro as avro
 import pandas as pd
 import pandera as pa
 from box import Box
@@ -13,3 +13,9 @@ def pa_schema(yaml_schema) -> pa.DataFrameSchema:
 def pa_validate(df: pd.DataFrame, schema: pa.DataFrameSchema) -> pd.DataFrame:
     logging.info("Validating Pandas DataFrame using Pandera DataFrameSchema.")
     return schema.validate(df)
+
+
+def df_avro_validate(df: pd.DataFrame, avsc: dict) -> bool:
+    return avro.validation.validate_many(
+        records=df.replace(pd.NA, "").to_dict(orient="records"), schema=parsed_schema
+    )
