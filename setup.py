14e72a0ca0e37478f4403228d15168a1f192d600
Steve Taylor <staylorx@gmail.com>
2020-10-23 14:23:20 -0600

    Setup to 0.13.12

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if os.path.isfile(requirement_path):
 # This call to setup() does all the work
 setuptools.setup(
     name="cupyopt",
-    version="0.13.6.1",
+    version="0.13.12.2",
     description="CU Python Opinionated Prefect Tasks",
     long_description=README,
     long_description_content_type="text/x-rst",
