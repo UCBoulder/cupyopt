8855dbd56e4988733bee8db49fa4fea7fdcaade5
d33bs <d33bs@users.noreply.github.com>
2020-10-27 09:30:15 -0600

    Removing pandera, adding PdColRenameAndFilter

--- src/cupyopt/validation.py
+++ src/cupyopt/validation.py
@@ -1,18 +1,8 @@
 import logging
+
 import fastavro as avro
 import pandas as pd
-import pandera as pa
 from box import Box
-
-
-def pa_schema(yaml_schema) -> pa.DataFrameSchema:
-    logging.info("Creating Pandera DataFrameSchema from Yaml-based schema.")
-    return pa.io.from_yaml(yaml_schema)
-
-
-def pa_validate(df: pd.DataFrame, schema: pa.DataFrameSchema) -> pd.DataFrame:
-    logging.info("Validating Pandas DataFrame using Pandera DataFrameSchema.")
-    return schema.validate(df)
 
 
 def df_avro_validate(df: pd.DataFrame, avsc: dict) -> bool:
