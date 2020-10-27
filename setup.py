437e3fb99c13bfc8db3941652ab7c9f1a583450a
d33bs <d33bs@users.noreply.github.com>
2020-10-27 12:31:45 -0600

    Adding the version in alignment with tag v0.13.12.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if os.path.isfile(requirement_path):
 # This call to setup() does all the work
 setuptools.setup(
     name="cupyopt",
-    version="0.13.12.2",
+    version="0.13.12.4",
     description="CU Python Opinionated Prefect Tasks",
     long_description=README,
     long_description_content_type="text/x-rst",
