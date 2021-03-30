5fe66d363d8f5e4a19ac1ed6cefd9c0b948b7181
d33bs <d33bs@users.noreply.github.com>
2021-03-30 16:31:02 -0600

    infer from df and write arrow schema

--- src/cupyopt/nuggets/schema.py
+++ src/cupyopt/nuggets/schema.py
@@ -1,11 +1,12 @@
 """ Schema functions """
 import json
-from typing import Union
 import logging
+from typing import Union
 
 import fastavro as avro
 import pandas as pd
 import pandavro as pda
+import pyarrow as pa
 
 logger = logging.getLogger(__name__)  # pylint: disable=C0103
 
@@ -22,6 +23,29 @@ def avro_schema(avsc: Union[dict, str]) -> dict:
         avsc = avro.schema.load_schema(avsc)
 
     return avsc
+
+
+def infer_df_arrow_schema(dataframe: pd.DataFrame) -> pa.lib.Schema:
+    """ Infer arrow schema from pandas dataframe """
+    logging.info("Inferring arrow schema from dataframe")
+    # infer the schema using pyarrow
+    arsc = pa.Schema.from_pandas(df=dataframe)
+
+    return arsc
+
+
+def arrow_schema_to_file(
+    arsc: pa.lib.Schema, filename: str = None, filedir: str = "./"
+) -> str:
+    """ Export arrow schema to file """
+    logging.info("Exporting arrow schema to file")
+
+    filepath = "{}{}.arsc".format(filedir, filename)
+
+    with open(filepath, "w") as arrow_file:
+        arrow_file.write(arsc.to_string())
+
+    return filepath
 
 
 def infer_df_avro_schema(
